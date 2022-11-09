430e5b3e206563543b5edb8680aa8937b4a9556d
n3navizhy <93191625+n3navizhy@users.noreply.github.com>
2022-11-09 19:48:41 +0300

    Если еще раз вернут, я честно повешусь

--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,15 @@
 from pathlib import Path
 from urllib.parse import urljoin
 MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEPS_URL = "https://peps.python.org/"
+DOWNLOAD_URL = urljoin(MAIN_DOC_URL, 'download.html')
+WHATSNEW_URL = urljoin(MAIN_DOC_URL, 'whatsnew/')
 BASE_DIR = Path(__file__).parent
 FILE_PATH = BASE_DIR/'results'/'pep_result.csv'
-DOWNLOAD_URL = urljoin(MAIN_DOC_URL, 'download.html')
-WN_URL = urljoin(MAIN_DOC_URL, 'whatsnew/')
+DOWNLOAD_DIR = BASE_DIR/'downloads'
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 PRETTY_MODE = 'pretty'
 FILE_MODE = 'file'
-DOWNLOAD_DIR = BASE_DIR/'downloads'
-PEPS_URL = "https://peps.python.org/"
-DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
-PEP_link = 'https://peps.python.org/'
 EXPECTED_STATUS = {
     'A': ('Active', 'Accepted'),
     'D': ('Deferred',),
--- src/main.py
+++ src/main.py
@@ -8,44 +8,44 @@ from requests import RequestException
 from tqdm import tqdm
 from urllib.parse import urljoin
 
-from constants import BASE_DIR, MAIN_DOC_URL, PEP_link, EXPECTED_STATUS
-from constants import FILE_PATH, DOWNLOAD_URL, PEPS_URL, WN_URL
+from constants import BASE_DIR, MAIN_DOC_URL, EXPECTED_STATUS
+from constants import FILE_PATH, DOWNLOAD_URL, PEPS_URL, WHATSNEW_URL
 from configs import configure_argument_parser, configure_logging
 from outputs import control_output
 from utils import get_soup, find_tag, logging_print
 
 LOGIING_ARCHIVE = 'Архив был загружен и сохранён:{path}'
 LOGIING_FILE = 'Файл с результатами был сохранён: {path}'
-LOGIING_PEP = 'Несовпадающие статусы: {link} \nСтатус в карточке: {p_status}' \
-              '\nОжидаемые статусы:{status} '
+LOGIING_PEP = 'Несовпадающие статусы: {link} \nСтатус в карточке: {p_status} '
+'\nОжидаемые статусы:{status} '
 LOGIING_SOUP = 'не удалось получить данные из {link}'
 
 
 def whats_new(session):
-    LOGGING_PULL = []
-    soup = get_soup(session, WN_URL)
-    li_list = soup.select('#what-s-new-in-python div.toctree-wrapper'
-                          'li.toctree-l1')
+    logging_pull = []
+    soup = get_soup(session, WHATSNEW_URL)
+    div_list = soup.find('div', class_='toctree-wrapper compound')
+    li_list = div_list.find_all('li', class_="toctree-l2")
     results = [('Ссылка на статью', 'Заголовок', 'Редактор, Автор'), ]
     for li in tqdm(li_list):
         li_tag = find_tag(li, 'a')
-        version_link = urljoin(WN_URL, li_tag['href'])
+        version_link = urljoin(WHATSNEW_URL, li_tag['href'])
         try:
             soup = get_soup(session, version_link)
         except RequestException:
-            LOGGING_PULL.append(LOGIING_SOUP.format(version_link))
+            logging_pull.append(LOGIING_SOUP.format(version_link))
             continue
         h1 = soup.find('h1').text
         d1 = soup.find('dl')
         d1_text = d1.text.replace('\n', ' ')
         results.append((li_tag['href'], h1, d1_text))
-    logging_print(LOGGING_PULL)
+    logging_print(logging_pull)
     return results
 
 
 def latest_versions(session):
-    soup = get_soup(session, MAIN_DOC_URL)
-    sidebar = find_tag(soup, 'div', attrs={'class': 'sphinxsidebar'})
+    sidebar = find_tag(get_soup(session, MAIN_DOC_URL), 'div',
+                       attrs={'class': 'sphinxsidebar'})
     ui_tags = sidebar.find_all('ul')
 
     for ul in ui_tags:
@@ -75,8 +75,8 @@ def latest_versions(session):
 
 def download(session):
     DOWNLOAD_DIR = BASE_DIR / 'downloads'
-    soup = get_soup(session, DOWNLOAD_URL)
-    table = find_tag(soup, 'table', attrs={'class': 'docutils'})
+    table = find_tag(get_soup(session, DOWNLOAD_URL), 'table',
+                     attrs={'class': 'docutils'})
     pdf = find_tag(table, 'a', {'href': re.compile(r'.+pdf-a4\.zip$')})
     link = urljoin(DOWNLOAD_URL, pdf['href'])
     file_name = link.split('/')[-1]
@@ -90,7 +90,7 @@ def download(session):
 
 
 def pep(session):
-    LOGGING_PULL = []
+    logging_pull = []
     counter = {
         'A': 0,
         'D': 0,
@@ -104,9 +104,9 @@ def pep(session):
 
     soup = get_soup(session, PEPS_URL)
     section = soup.find('section', id='index-by-category')
-    section = section.find_all('section')
+    sections = section.find_all('section')
 
-    for sec in tqdm(section):
+    for sec in tqdm(sections):
         table = sec.find('table')
         if table is None:
             continue
@@ -115,11 +115,11 @@ def pep(session):
             tag = find_tag(pep, 'td')
             link = find_tag(tag.find_next_sibling(), 'a')['href']
             tag = tag.text[1:]
-            ab_link = urljoin(PEP_link, link)
+            ab_link = urljoin(PEPS_URL, link)
             try:
                 soup = get_soup(session, ab_link)
             except RequestException:
-                LOGGING_PULL.append(LOGIING_SOUP.format(link=ab_link))
+                logging_pull.append(LOGIING_SOUP.format(link=ab_link))
                 continue
             content = find_tag(soup, 'section',
                                attrs={'id': "pep-content"})
@@ -133,19 +133,16 @@ def pep(session):
             if p_status in EXPECTED_STATUS[tag]:
                 counter[tag] += 1
             else:
-                LOGGING_PULL.append(LOGIING_PEP.format(link=link,
+                logging_pull.append(LOGIING_PEP.format(link=link,
                                                        p_status=p_status,
                                                        status=EXPECTED_STATUS[
                                                            tag]))
-    with open(FILE_PATH, 'w', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, counter.keys())
-        writer.writeheader()
-        writer.writerow(counter)
-        writer = csv.writer(file, delimiter=' ')
-        writer.writerow(["Total: " + str(sum(counter.values()))])
-    logging_print(LOGGING_PULL)
-    logging.info(LOGIING_FILE.format(path=FILE_PATH))
-
+    logging_print(logging_pull)
+    results = [("Статус", "Количество")]
+    for key, value in counter.items():
+        results.append((key, value))
+    results.append(("Total ", sum(counter.values())))
+    return results
 
 MODE_TO_FUNCTION = {
     'whats-new': whats_new,
@@ -153,6 +150,8 @@ MODE_TO_FUNCTION = {
     'download': download,
     'pep': pep,
 }
+
+MAIN_ERROR_MESSAGE = 'Возникла ошибка: {error_message}'
 
 
 def main():
@@ -169,8 +168,9 @@ def main():
         if results is not None:
             control_output(results, args)
         logging.info('Парсер завершил работу.')
-    except Exception:
-        logging.exception('Возникла ошибка\n', Exception, stack_info=True)
+    except Exception as error:
+        logging.exception(MAIN_ERROR_MESSAGE.format(error_message=error),
+                          stack_info=True)
 
 
 if __name__ == '__main__':
